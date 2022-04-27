29103ecda0e6c7cc3ca64703956d108d2e231750
Tomáš Korenko <radixxko@gmail.com>
2022-04-27 16:00:49 +0200

    pdf test

--- test/test.js
+++ test/test.js
@@ -101,9 +101,9 @@ async function renderTest()
         const fs = require('fs'), PDF = require('../../../liqd-js/pdf');
         const Invoice = new PDF( require('fs').readFileSync( __dirname + '/pdf.html', 'utf8' ));
 
-        let document = await billing.document.get( 'MSK22002065' );
+        let document = await billing.document.get( 'MSK22000006' );
 
-        Invoice.render( document, { locale: 'sk' });
+        Invoice.render( document, { locale: 'sk' }, __dirname + '/invoice.pdf');
 
         console.log( document );
     }
@@ -113,6 +113,6 @@ async function renderTest()
     }
 }
 
-importTest();
+//importTest();
 
-//renderTest();
+renderTest();
